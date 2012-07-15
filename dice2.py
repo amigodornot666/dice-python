15a2f59a84ef12e66256d826202234eae3545ed9
kyle roux <decimalwarrior@gmail.com>
2012-07-15 15:06:17 -0700

    gor the loop workin

--- dice2.py
+++ dice2.py
@@ -27,73 +27,82 @@ list6 = [a1,a2,a3,a4,a5,a6]
 # we need to seed so we actually get a random number
 random.seed()
 
-# now we roll the dice
-dce1 = random.choice(list1)
-dce2 = random.choice(list2)
-dce3 = random.choice(list3)
-dce4 = random.choice(list4)
-dce5 = random.choice(list5)
-dce6 = random.choice(list6)
+PLAY = 'y'
 
-dice_list= [dce1,dce2,dce3,dce4,dce5,dce6]
+while PLAY == 'y':
+  # now we roll the dice
+  dce1 = random.choice(list1)
+  dce2 = random.choice(list2)
+  dce3 = random.choice(list3)
+  dce4 = random.choice(list4)
+  dce5 = random.choice(list5)
+  dce6 = random.choice(list6)
 
-xtra = len(set(dice_list)) <=2
+  dice_list = [dce1,dce2,dce3,dce4,dce5,dce6]
+  
 
+  # assign the scores
+  if dce1 == a1: 
+    D1 = 100
+  elif dce1 == a5:
+    D1 = 50
+  else: 
+    D1 = 0
 
-# assign the scores
-if dce1 == a1: 
-  D1 = 100
-elif dce1 == a5:
-  D1 = 50
-else: 
-  D1 = 0
+  if dce2 == a1: 
+    D2 = 100
+  elif dce2 == a5:
+    D2 = 50
+  else: 
+    D2 = 0
 
-if dce2 == a1: 
-  D2 = 100
-elif dce2 == a5:
-  D2 = 50
-else: 
-  D2 = 0
+  if dce3 == a1: 
+    D3 = 100
+  elif dce3 == a5:
+    D3 = 50
+  else: 
+    D3 = 0
 
-if dce3 == a1: 
-  D3 = 100
-elif dce3 == a5:
-  D3 = 50
-else: 
-  D3 = 0
+  if dce4 == a1: 
+    D4 = 100
+  elif dce4 == a5:
+    D4 = 50
+  else: 
+    D4 = 0
 
-if dce4 == a1: 
-  D4 = 100
-elif dce4 == a5:
-  D4 = 50
-else: 
-  D4 = 0
+  if dce5 == a1: 
+    D5 = 100
+  elif dce5 == a5:
+    D5 = 50
+  else: 
+    D5 = 0
 
-if dce5 == a1: 
-  D5 = 100
-elif dce5 == a5:
-  D5 = 50
-else: 
-  D5 = 0
-
-if dce6 == a1: 
-  D6 = 100
-elif dce6 == a5:
-  D6 = 50
-else: 
-  D6 = 0
-
-
-
-Blank = commands.getoutput("clear")
-
-print Blank
+  if dce6 == a1: 
+    D6 = 100
+  elif dce6 == a5:
+    D6 = 50
+  else: 
+    D6 = 0
 
 
-print xtra
-print dce1, " ", dce2, " ", dce3, " ", dce4, " ", dce5, " ", dce6
 
-print D1, " ", D2, " ", D3, " ", D4, " ", D5, " ", D6
-TOTAL = D1 + D2 + D3 + D4 + D5 + D6
+  #Blank = commands.getoutput("clear")
 
-print 'your total is', TOTAL
+  #print Blank
+
+
+  #print xtra
+  print dce1, " ", dce2, " ", dce3, " ", dce4, " ", dce5, " ", dce6
+  print D1, " ", D2, " ", D3, " ", D4, " ", D5, " ", D6
+  TOTAL = D1 + D2 + D3 + D4 + D5 + D6
+
+  print 'your total is', TOTAL
+  print 'Would you like to play again (y or n)?'
+  
+  PLAY = raw_input()
+
+
+print 'Thanks for playing, have a great day!!'
+
+
+
